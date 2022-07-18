7662939583ed26068254c778db9da39aa02e032e
Jon Kaplon <jon.kaplon1@gmail.com>
2022-07-18 08:52:37 -0700

    is-lucky

---
+++ isLucky.js
@@ -0,0 +1,39 @@
+function solution(n) {
+  let arr = Array.from(String(n));
+
+  first = [];
+  second = [];
+
+  totalFirst = 0;
+  totalSecond = 0;
+
+  for (let i = 0; i < arr.length / 2; i++) {
+    first.push(parseInt(arr[i]));
+  }
+
+  for (let i = arr.length - 1; i > arr.length / 2 - 1; i--) {
+    second.push(parseInt(arr[i]));
+  }
+
+  console.log(arr);
+  console.log(first);
+  console.log(second);
+
+  totalFirst = first.reduce((a, b) => a + b, 0);
+  totalSecond = second.reduce((a, b) => a + b, 0);
+
+  console.log(totalFirst);
+  console.log(totalSecond);
+
+  if (totalFirst === totalSecond) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const input1 = 1230;
+const input2 = 239017;
+
+console.log(solution(input1));
+console.log(solution(input2));
