ca230fc34860a92ffc62a8045736002bed310925
Jon Kaplon <jon.kaplon1@gmail.com>
2022-08-05 10:03:38 -0700

    line-encoding

---
+++ isDigit.js
@@ -0,0 +1,32 @@
+// Determine if the given character is a digit or not.
+
+// Example
+
+// For symbol = '0', the output should be
+// solution(symbol) = true;
+// For symbol = '-', the output should be
+// solution(symbol) = false.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] char symbol
+
+// A character which is either a digit or not.
+
+// Guaranteed constraints:
+// Given symbol is from ASCII table.
+
+// [output] boolean
+
+// true if symbol is a digit, false otherwise.
+
+function solution(symbol) {
+  return /[0-9]/.test(symbol);
+}
+
+s = 0;
+s1 = "-";
+
+console.log(solution(s));
+console.log(solution(s1));
